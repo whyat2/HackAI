e42f286dbae6af70397411d2cdcc84e9d7915fd7
whyat2 <141190141+whyat2@users.noreply.github.com>
2025-04-19 23:37:55 -0500

    Pushing up the corrected backend

--- Backend/productionFlaskStuff/getQueryInfo.py
+++ Backend/productionFlaskStuff/getQueryInfo.py
@@ -6,6 +6,7 @@ import google.generativeai as genai
 import google.generativeai as genai
 from google.api_core import retry
 
+load_dotenv()
 GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
 genai.configure(api_key=GOOGLE_API_KEY)
 chromaClient = chromadb.PersistentClient()
